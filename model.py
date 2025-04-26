975a0a44f99a08f21a36e175248623b6777559ef
VictorDu98 <toandu.business@gmail.com>
2025-04-27 01:28:22 +0700

    Model - changed method createConfig to create_preset

--- model.py
+++ model.py
@@ -14,21 +14,24 @@ class Task:
         self.P4USER = p4_user
         self.P4CHARSET= p4_charset
         self.P4CLIENT = p4_client
-        self.createConfig()
+        self.create_preset()
 
     def __del__(self):
         if os.path.exists(self.task_name):
             os.remove(self.task_name)
             print(f"Deleted file: {self.task_name}")
 
-    def runTask(self):
+    def run_task(self):
         #execute
         #return 0-1
         pass
 
-    def createConfig(self):
-        filename = f".p4config_{self.task_name}.txt"
-        with open(filename, 'w') as file:
+    def create_preset(self):
+        p4_config_name = f".p4config_{self.task_name}.txt"
+        user_list = f"users_{self.task_name}.json"
+        log = f"log_{self.task_name}.json"
+
+        with open(p4_config_name, 'w') as config_file:
             config = f"""
 P4PORT={self.P4PORT}
 P4USER={self.P4USER}
@@ -36,9 +39,15 @@ P4CLIENT={self.P4CLIENT}
 P4CHARSET={self.P4CHARSET}
 P4CONFIG=            
             """
-            file.write(config)
+            config_file.write(config)
 
-    def useConfig(self,task_name):
+        with open(user_list, 'w') as users_file:
+            users_file.write("user")
+
+        with open(log, 'w') as log_file:
+            log_file.write("log")
+
+    def use_config(self,task_name):
         #from P4 import P4, P4Exception
         #p4.config=task_name
         pass

eca42bfb81f5ffa133c7ff8352d6fb5fcc1a85e3
VictorDu98 <toandu.business@gmail.com>
2025-05-03 11:35:53 +0700

    Model- defined core algorithm

--- model.py
+++ model.py
@@ -2,11 +2,14 @@ import time
 import os
 import subprocess
 import shutil
-from os.path import exists
-
 import requests
 import json
 import re
+from datetime import datetime
+from P4 import P4, P4Exception
+
+formatted_date = datetime.now().strftime("%y%m%d")
+
 target_time = "15:06"
 
 TOTAL_TASK=0
@@ -18,11 +21,18 @@ class Preset:
         self.name = name
         self.preset_root = os.path.join(PRESET_DIR, name)
         self.preset_config = os.path.join(self.preset_root, f"config_{self.name}.json")
-        self.perforce_config = os.path.join(self.preset_root, f"p4config_{self.name}.txt")
+        self.perforce_config = os.path.join(self.preset_root, f".p4config_{self.name}.txt")
 
         if not os.path.exists(self.preset_root):
             os.makedirs(self.preset_root, exist_ok=True)
             self.create_template()
+
+        JSON = self.open_json()
+        self.output_path = JSON["misc"]["Address"]
+        self.slack_uri = JSON["misc"]["Slack"]
+        self.producer_slack_id = JSON["misc"]["Producer"]
+        self.output_log = None
+        self.output_report = None
 
     def __str__(self):
         return self.name
@@ -36,7 +46,7 @@ class Preset:
 
     def create_template(self):
         dic = {
-            "output":[
+            "misc":[
                 {
                     "Address": "Project address on server network",
                     "Slack": "Slack incoming webhook",
@@ -93,28 +103,12 @@ class Preset:
             file.write(config)
 
     def open_json(self):
+        if not self.check_exist(self.preset_config):
+            raise FileNotFoundError(f"File {self.preset_config} does not exist.")
         f = open(self.preset_config, "r", encoding="utf-8")
         data = json.load(f)
         f.close()
         return data
-
-    def login(self):
-        if not self.check_exist(self.perforce_config):
-            raise FileNotFoundError(f"File {self.perforce_config} does not exist.")
-
-        from P4 import P4, P4Exception
-        p4 = P4()
-        p4.config = self.perforce_config
-
-        try:  # Catch exceptions with try/except
-            p4.connect()  # Connect to the Perforce server
-            info = p4.run("info")  # Run "p4 info" (returns a dict)
-            for key in info[0]:  # and display all key-value pairs
-                print(key, "=", info[0][key])
-
-        except P4Exception:
-            for e in p4.errors:  # Display errors
-                print(e)
 
     @staticmethod
     def filter_log(string_list:list)-> set:
@@ -124,8 +118,8 @@ class Preset:
         Lines of text that matched regex will be returned in a set array\n
         Lines of text that fit in ignore case will be ignored since the file itself it's not on depot yet
 
-        :param string_list: read bunch of string line
-        :return: workspace name list that match regex expression
+        :param string_list: string line
+        :return: workspace name list that matches regex expression
         """
         pattern= r"by\s+(.*?)(\s+\*exclusive\*)?(\s+\*locked\*)?$"
         ignore_mark_for_add = r".*add.*"
@@ -138,9 +132,43 @@ class Preset:
                     # Split string with delimiter "@" and take workspace name by index 1
                     workspace_name = string.split("@")[1]
                     workspaces.add(workspace_name)
+
         return workspaces
 
+    def generate_log(self):
+        JSON= self.open_json()
+        accounts_name = JSON["info"]["AccountName"]
 
+        if not self.check_exist(self.perforce_config):
+            raise FileNotFoundError(f"File {self.perforce_config} does not exist.")
+
+        self.output_log = os.path.join(self.output_path,"logs", f"log_{formatted_date}.txt")
+
+        if self.check_exist(self.output_log):
+            os.remove(self.output_log)
+
+        try:  # Catch exceptions with try/except
+            p4 = P4()
+            p4.p4config_file = self.perforce_config
+
+            p4.connect()  # Connect to the Perforce server
+            p4.run_login()
+        except P4Exception:
+            for e in p4.errors:  # Display errors
+                raise e
+
+        for name in accounts_name:
+            # Run the `p4 opened -u <user>` command
+            opened_files = p4.run_opened("-u", name)
+
+            if opened_files:
+                with open(self.output_log, 'a', encoding='utf-8') as f:
+                    f.write(f"-------------------------{name}----------------------------\n")
+                    # Only write lines that contain the word "edit"
+                    for line in output.splitlines():
+                        if "edit" in line:
+                            f.write(line + "\n")
+                    f.write("\n")
 
     def filter_user(self):
         # Assume FilterLogFile() is defined elsewhere and returns a list of found workspaces.
@@ -159,16 +187,16 @@ class Preset:
 
         return json_indexes
 
-    def generate_report(self,json_indexes:list,department:str,output_full_path:str):
-        if self.check_exist(output_full_path):
-            os.remove(output_full_path)
+    def generate_report(self,json_indexes:list,department:str):
+        self.output_report = os.path.join(self.output_path, "logs", f"check_out_report_{department}_{formatted_date}.txt")
+        if self.check_exist(self.output_report):
+            os.remove(self.output_report)
 
         # Read and parse the JSON file
         JSON = self.open_json()
         for index in json_indexes:
             if JSON['info']['Department'][index] == department:
-                report = (f"{JSON['info']['Project'][index]} - {JSON['info']['UserName'][index]} - "
-                    f"{JSON['info']['WorkSpace'][index]} - {JSON['info']['Email'][index]}")
-                with open(output_full_path, 'a', encoding='utf-8') as f:
+                report = f"{JSON['info']['Project'][index]} - {JSON['info']['UserName'][index]} - {JSON['info']['WorkSpace'][index]} - {JSON['info']['Email'][index]}"
+                with open(self.output_report, 'a', encoding='utf-8') as f:
                     f.write(report + "\n")
 

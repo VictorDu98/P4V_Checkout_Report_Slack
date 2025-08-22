a9140b3ec5a768763d698cfe2314d9511c7661e7
Du Van Toan <toandu.business@gmail.com>
2025-08-22 15:19:01 +0700

    Added doc string for start.py

--- start.py
+++ start.py
@@ -1,6 +1,14 @@
 import time
 import model
 
+"""
+        Make sure to have every class instance .p4tickets generated, so the script is not blocked by waiting user to login.
+        Any class instance has .p4tickets of it own expired will not stop the script, it will be skipped and printed error on terminal for debug purpose.
+                Try to delete the .p4tickets and run that class instance separately to generate a new one.
+                The current script terminal will use the new .p4tickets for the next time class instance is triggered.
+        Pass the time into main function to set trigger time.
+"""
+
 def main(*args):
     while True:
         current_time = time.strftime("%H:%M")
